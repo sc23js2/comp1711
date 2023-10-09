0e005d7e1e05c84782fd376b9a38f79d2c2be35e
Jasmine Sewell <146713369+sc23js2@users.noreply.github.com>
2023-10-09 12:21:32 +0000

    WEEK2

--- WEEK2/worksheet1.c
+++ WEEK2/worksheet1.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-void one(){
+#include <string.h>
+void one(){ //hello world
     printf("Hello World");
 }
 
-void two(){
+void two(){ //interger sum
     int a, b, c;
     printf("Enter number 1: ");
     scanf("%d", &a);
@@ -14,8 +15,82 @@ void two(){
     printf("Result is %d\n", c);
 }
 
-void three(){
-    printf("MAX of int %d", __INT16_MAX__)
+void three(){ //output max vaue of eacj datatype
+    printf("MAX of int %d", __INT16_MAX__);
+}
+
+
+void four(){ //area of a circle
+    float radius, area, pi;
+    printf("Enter circle radius: ");
+    scanf("%f", radius);
+
+    pi = 2.141529;
+    area = pi * radius * radius;
+    printf("Area of circle is %f", area);
+
+}
+
+void five(){ //swapping variables
+    char a[] = "variable 1";
+    char b[] = "variable 2";
+    char temp[] = "";
+
+   // temp = strcpy(a);
+    // a = b;
+    // b = a;
+}
+
+void six(){ //odd or even
+
+    int number;
+    printf("Enter a number: ");
+    scanf("%d", number);
+    
+    if (number % 2 == 0) {
+        printf("%d is an even number");
+    }
+    else{
+        printf("%d is an odd number");
+    }
+}
+
+void seven(){ //farenheight to celcuius
+
+    float degreesF, degreesC, conversion;
+    prinf("Enter farenheight degrees");
+    scanf("%f", degreesF);
+    degreesC = (degreesF - 32)/1.8;
+    printf("%f degrees farenheight is %f degrees celcius", degreesF, degreesC);
+}
+
+void eight(){ //factorial
+
+    int num, result;
+    printf("Enter a number: ");
+    scanf("%d", num);
+     
+    result = factorial(num);
+    printf("factoril of %d is %d", num, result);
+}
+
+void nine(){  //
+
+}
+
+void ten(){ //
+
+}
+
+
+int factorial(num)
+{
+    if (num > 1){
+        return num * factorial(num-1);
+    }
+    else{
+        return num;
+    }
 }
 
 int main() {
