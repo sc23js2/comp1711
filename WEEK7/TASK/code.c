0111cc004c28dfccce76c3f880c30c449bf423e5
Jasmine Sewell <146713369+sc23js2@users.noreply.github.com>
2023-11-13 10:54:53 +0000

    WEEK7

---
+++ WEEK7/TASK/code.c
@@ -0,0 +1,136 @@
+#include "utilities.h"
+#include <ctype.h>  //<-- You need this to use toupper() function.
+int main()
+{
+    // array of daily readings
+    reading daily_readings[100];
+
+    char line[buffer_size];
+    char filename[buffer_size];
+
+    // get filename from the user
+    printf("Please enter the name of the data file: ");
+
+    // these lines read in a line from the stdin (where the user types)
+    // and then takes the actual string out of it
+    // this removes any spaces or newlines.
+    fgets(line, buffer_size, stdin);
+    sscanf(line, " %s ", filename);
+
+    char choice;
+    int counter = 0;
+    float mean = 0;
+
+    while (1)
+    {
+        FILE *input = fopen(filename, "r");
+        if (!input)
+        {
+            printf("Error: File could not be opened\n");
+            return 1;
+        }
+        printf("\n");
+        printf("A: View all your blood iron levels\n");                       // BRONZE
+        printf("B: View your average blood iron level\n");                    // BRONZE
+        printf("C: View your lowest blood iron level\n");                     // SILVER
+        printf("D: View your highest blood iron level\n");                    // SILVER
+        printf("E: View the blood iron levels for a specific month\n");       // SILVER/GOLD
+        printf("F: See some additional statistics about your iron levels\n"); // GOLD - see readme.md
+        printf("G: Generate a graph of your iron levels\n");                  // GOLD/PLATINUM - see readme.md
+        printf("Q: Exit the program\n");
+        printf("Enter option: ");
+        // get the next character typed in and store in the 'choice'
+        choice = getchar();
+
+        // this gets rid of the newline character which the user will enter
+        // as otherwise this will stay in the stdin and be read next time
+        while (getchar() != '\n');
+        choice = toupper(choice);
+
+        // switch statement to control the menu.
+        switch (choice)
+        {
+        // this allows for either capital or lower case
+        case 'A':
+            counter = 0;
+            while (fgets(line, buffer_size, input))
+            {
+                // split up the line and store it in the right place
+                // using the & operator to pass in a pointer to the bloodIron so it stores it
+                tokeniseRecord(line, ",", daily_readings[counter].date, &daily_readings[counter].bloodIron);
+                counter++;
+            }
+            for (int i = 0; i < counter; i++)
+            {
+                printf("%s - Blood iron: %.1f\n", daily_readings[i].date, daily_readings[i].bloodIron);
+            }
+            fclose(input);
+            break;
+
+        case 'B':
+            counter = 0;
+            while (fgets(line, buffer_size, input))
+            {
+                // split up the line and store it in the right place
+                // using the & operator to pass in a pointer to the bloodIron so it stores it
+                tokeniseRecord(line, ",", daily_readings[counter].date, &daily_readings[counter].bloodIron);
+                mean += daily_readings[counter].bloodIron;
+                counter++;
+            }
+            mean /= counter;
+            printf("Your average blood iron was %.2f\n", mean);
+            fclose(input);
+            break;
+
+        case 'C':
+            while (fgets(line, buffer_size, input))
+            {
+                // split up the line and store it in the right place
+                // using the & operator to pass in a pointer to the bloodIron so it stores it
+                tokeniseRecord(line, ",", daily_readings[counter].date, &daily_readings[counter].bloodIron);
+                counter++;
+            }
+            printf("%f", find_lowest(daily_readings, counter));
+            break;
+
+        case 'D':
+            while (fgets(line, buffer_size, input))
+            {
+                // split up the line and store it in the right place
+                // using the & operator to pass in a pointer to the bloodIron so it stores it
+                tokeniseRecord(line, ",", daily_readings[counter].date, &daily_readings[counter].bloodIron);
+                counter++;
+            }
+            printf("%f", find_highest(daily_readings, counter));
+            break;
+
+        case 'E':
+        while (fgets(line, buffer_size, input))
+            {
+                // split up the line and store it in the right place
+                // using the & operator to pass in a pointer to the bloodIron so it stores it
+                tokeniseRecord(line, ",", daily_readings[counter].date, &daily_readings[counter].bloodIron);
+                counter++;
+            }
+            monthly_iron(daily_readings, counter);
+            break;
+
+        case 'F':
+            return 0;
+            break;
+
+        case 'G':
+            return 0;
+            break;
+
+        case 'Q':
+            return 0;
+            break;
+
+        // if they type anything else:
+        default:
+            printf("Invalid choice\n");
+            break;
+        }
+    }
+}
